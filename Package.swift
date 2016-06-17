76ef6410c8ebeb53f2b77ceace1dbe8303cdf504
Jan <mail@mediatainment-productions.com>
2016-06-17 16:50:09 +0200

    2.3.2 instead of 2.3.3

--- Package.swift
+++ Package.swift
@@ -1,6 +1,6 @@
 let package = Package(
     name: "SwiftyJSON",
     dependencies: [
-        .Package(url: "https://github.com/mediatainment/SwiftyJSON.git", versions: Version(1, 0, 0)..<Version(2,3,3))
+        .Package(url: "https://github.com/mediatainment/SwiftyJSON.git", versions: Version(1, 0, 0)..<Version(2,3,2))
     ]
 )
