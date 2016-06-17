3b02253921ce6db1e51d446da68662091715a1aa
Jan <mail@mediatainment-productions.com>
2016-06-17 17:07:03 +0200

    maybe with name only?

--- Package.swift
+++ Package.swift
@@ -1,8 +1,5 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftyJSON",
-    dependencies: [
-        .Package(url: "https://github.com/mediatainment/SwiftyJSON.git", versions: Version(1, 0, 0)..<Version(2,3,2))
-    ]
+    name: "SwiftyJSON"
 )
